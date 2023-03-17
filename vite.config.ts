4980f016692d7b00acd1233f82141b2f30a6b487
Chuck Sailer <csailer@acadian-asset.com>
2023-03-17 10:24:52 -0400

    configured vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,40 @@
 import * as path from 'path';
 import react from '@vitejs/plugin-react';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
-
 import manifest from './manifest.json';
 
-// https://vitejs.dev/config/
+// Add the plugin to the plugins array in the vite.config.js
 export default defineConfig({
   plugins: [
-    react(),
+    reactRefresh(),
+    tsconfigPaths(),
     VitePWA({
-      manifest,
-      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
-      // switch to "true" to enable sw on development
-      devOptions: {
-        enabled: false,
+      manifest: {
+        "name": "IDC Okta API",
+        "short_name": "IDC Okta",
+        "description": "A PWA for the IDC Okta API application.",
+        "start_url": "/",
+        "display": "standalone",
+        "background_color": "#ffffff",
+        "theme_color": "#000000",
+        "icons": [
+          {
+            "src": "/path/to/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "/path/to/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          }
+        ]
       },
       workbox: {
-        globPatterns: ['**/*.{js,css,html}', '**/*.{svg,png,jpg,gif}'],
+        globPatterns: ['**/*.{ts,jsx,tsx,js,css,html}', '**/*.{svg,png,jpg,gif}'],
       },
     }),
   ],
@@ -27,3 +44,13 @@ export default defineConfig({
     },
   },
 });
+
+
+
+
+
+
+
+
+
+
